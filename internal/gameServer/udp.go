87fddda7ff2665e6f770d6ac4d739e15401eb0c5
Nayla Hanegan <naylahanegan@gmail.com>
2024-08-26 15:41:13 -0400

    test

--- internal/gameServer/udp.go
+++ internal/gameServer/udp.go
@@ -115,7 +115,7 @@ func (g *GameServer) processUDP(addr *net.UDPAddr, buf []byte) {
 		countLag := g.sendUDPInput(count, addr, playerNumber, spectator != 0, sendingPlayerNumber)
 
 		// Read BufferHealth from BufferSize first
-		g.GameData.BufferHealth[sendingPlayerNumber] = int32(g.GameData.BufferSize[sendingPlayerNumber])
+		g.GameData.BufferHealth[sendingPlayerNumber] = int32(g.GameData.BufferSize[sendingPlayerNumber]) + 2
 
 		g.GameDataMutex.Lock() // PlayerAlive can be modified by ManagePlayers in a different thread
 		g.GameData.PlayerAlive[sendingPlayerNumber] = true
@@ -184,7 +184,7 @@ func (g *GameServer) createUDPServer() error {
 
 	g.GameData.PlayerAddresses = make([]*net.UDPAddr, 4) //nolint:gomnd
 	g.GameData.BufferSize = []uint32{3, 3, 3, 3}
-	g.GameData.BufferHealth = []int32{3, 3, 3, 3}
+	g.GameData.BufferHealth = []int32{5, 5, 5, 5}
 	g.GameData.Inputs = make([]map[uint32]uint32, 4) //nolint:gomnd
 	for i := 0; i < 4; i++ {
 		g.GameData.Inputs[i] = make(map[uint32]uint32)
