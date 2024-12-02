3d1500ac93a2020430f38c7d60015eaf3ccd1aa8
Nayla Hanegan <naylahanegan@gmail.com>
2024-12-01 19:14:58 -0500

    test

--- internal/gameServer/server.go
+++ internal/gameServer/server.go
@@ -131,7 +131,7 @@ func (g *GameServer) ChangeBuffer(buffer int) {
 	weightedBuffer := uint32(buffer)
 	for i := range g.GameData.BufferSize {
 		g.GameData.BufferSize[i] = weightedBuffer
-		g.GameData.BufferHealth[i] = int32(weightedBuffer) + int32(weightedBuffer)
+		g.GameData.BufferHealth[i] = int32(weightedBuffer) + int32(weightedBuffer) + int32(weightedBuffer) + int32(weightedBuffer)
 	}
 }
 
